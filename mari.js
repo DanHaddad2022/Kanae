95cdc9f52c8563a13c9b3f2322eb4203e4aa76e2
DanHaddad2022 <101983469+DanHaddad2022@users.noreply.github.com>
2023-05-21 19:20:45 -0300

    Create mari.js

---
+++ mari.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.querySelector('button');
+    button.addEventListener('click', function() {
+        alert('Eu te amo');
+    });
+});
